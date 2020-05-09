e695d393ef9515ab282e98010ba9310db8b8d2d2
Lukas Zierahn <lukas@kappa-mm.de>
2020-05-09 13:30:16 +0200

    Initial Commit

---
+++ Log.h
@@ -0,0 +1,17 @@
+//
+//  Log.h
+//  Log
+//
+//  Created by Lukas on 09.05.20.
+//  Copyright © 2020 Lukas. All rights reserved.
+//
+
+#ifndef Log_h
+#define Log_h
+
+#include "Render.hpp"
+#include "ModelLoader/ModelLoader.hpp"
+#include "Camera.hpp"
+#include "ModelLoader/ColoredObject.hpp"
+
+#endif /* Log_h */
